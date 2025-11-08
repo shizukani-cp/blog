d1387d1ccc3fa8a18a87c363cc5d16fc9e2074d8
shizukani-cp <shizukani.cp@gmail.com>
2025-11-09 07:44:17 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: branchName,
+  basePath: branchName,
 };
 
 export default nextConfig;
