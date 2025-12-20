6dc9852f78cbdf997ac796234650bdcf1d3ca18d
shizukani-cp <shizukani.cp@gmail.com>
2025-12-20 16:21:22 +0900

    ここを直せばいけると信じてみる

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: branchName,
-  basePath: branchName,
+  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
   trailingSlash: true,
   output: 'export',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
