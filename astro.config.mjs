6b45a6f5db8dc73d8cc1a9bf6aef6e2605c46cf4
shizukani-cp <shizukani.cp@gmail.com>
2026-06-08 17:16:44 +0900

    RSS & Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shizukani-cp.github.io',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
     },
   },
+
+  integrations: [sitemap()],
 });
