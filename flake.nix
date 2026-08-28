5cc54ce9dcb21e098bcd41ad81a5860a11f4686e
shizukani-cp <shizukani.cp@gmail.com>
2026-08-28 13:37:48 +0900

    自動でできないと意味ないよね

--- flake.nix
+++ flake.nix
@@ -1,22 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.mkShell {
+          packages = with pkgs; [
             tailwindcss-language-server
             typescript
             typescript-language-server
             vscode-langservers-extracted
           ];
         };
-      };
-    };
+      }
+    );
 }
